c313182e7a63a0a57f3d2ce22569b89e1f76b0c4
SimoneGasperini <simone.gasperini2@studio.unibo.it>
2021-02-28 10:48:56 +0100

    add readonly property to all txt files in database

--- setup.py
+++ setup.py
@@ -1,33 +1,41 @@
-from setuptools import setup
-from setuptools import find_packages
-
 import os
+from glob import glob
+from stat import S_IREAD, S_IRGRP, S_IROTH
 
 
-def get_requirements ():
-
-  with open(file='./requirements.txt', mode='r') as file:
-    reqs = file.read().splitlines()
-
-  return reqs
-
-
-def set_path ():
-
+def set_abspath ():
   with open(file='./rboost/cli/__path.py', mode='w') as file:
     line = 'PATH = ' + '"' + os.getcwd() + '"'
     file.write(line.replace('\\','/'))
 
 
 def download_wordnet ():
-
   import nltk
   nltk.download('wordnet')
 
 
+def make_readonly ():
+  txt_files = [obj for obj in glob('./rboost/database/**', recursive=True)
+               if os.path.isfile(obj) and obj.endswith('.txt')]
+  for file in txt_files:
+    os.chmod(file, S_IREAD|S_IRGRP|S_IROTH)
 
-set_path()
+
+set_abspath()
 download_wordnet()
+make_readonly()
+
+
+
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+def get_requirements ():
+  with open(file='./requirements.txt', mode='r') as file:
+    reqs = file.read().splitlines()
+  return reqs
 
 setup (
 
